919566524b73c8ce9b554d79912bc123a685aa5c
N Puneeth <puneeth97@gmail.com>
2014-01-27 22:45:40 +0530

    Finished the CompleteFunction

--- complete.R
+++ complete.R
@@ -1,15 +1,21 @@
-complete <- function(directory, id = 1:332) {
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+complete <- function(directory,id = 1:332) {
 
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        ## to be used
-        
-        ## Return a data frame of the form:
-        ## id nobs
-        ## 1  117
-        ## 2  1041
-        ## ...
-        ## where 'id' is the monitor ID number and 'nobs' is the
-        ## number of complete cases
-}
+   iid<-id
+
+   csvfiles <- sprintf("/Users/puneeth_nn/Documents/%s/%03d.csv", directory, as.numeric(iid))
+	print(csvfiles)
+   ##nrows <- sapply( csvfiles, function(f) nrow(read.csv(f)))
+   ##final<-read.csv(is.character(nrows))
+   ##omitted<-final[complete.cases(final),]
+	##rownew<-nrow(omitted)
+	##print(rownew)
+   nrows <- sapply( csvfiles, function(f) nrow(read.csv(f)))
+   ##sum(nrows)
+	##print(nrows)
+        rowlabels <- nrow(nrows)
+
+        data.frame(id=sprintf('%3d', iid), 
+            nobs=sapply(csvfiles,function(f) nrow(na.omit(read.csv(f))))
+		,row.names=rowlabels
+           )
+       }
